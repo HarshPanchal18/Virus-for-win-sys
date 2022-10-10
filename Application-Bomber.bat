80d17ceeab8d6d5378d096887d43e7fa4484dacb
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-10-10 14:22:44 +0530

    Create Application-Bomber.bat

---
+++ Application-Bomber.bat
@@ -0,0 +1,14 @@
+rem it will start to open different applications repeatedly which will affect the system performance
+rem you can also add the applications of your choice with the following command
+
+@echo off
+
+start winword
+start mspaint
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+goto x
