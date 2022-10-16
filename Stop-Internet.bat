0170793328a2dc561670981ef0f980a945eef26c
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-10-16 11:44:42 +0530

    Create Stop-Internet.bat

---
+++ Stop-Internet.bat
@@ -0,0 +1,7 @@
+@echo off
+
+ipconfig /release
+
+rem Send it to someone, There IP address will be lost, and therefore they won't be able to fix it.
+rem However, this is very easy to fix.
+rem Simply type IPconfig /renew
